b58e5004e359d2a72f763ddddbe249e9b30f7265
Francisco de Assis Marcelino <famarcelino@gmail.com>
2020-12-04 07:28:12 -0300

    Add ProductPrice tests

---
+++ front-web/src/core/components/ProductPrice/__tests__/ProductPrice.spec.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import ProductPrice from '..';
+
+test('should render ProductPrice', () => {
+    const price = 1200;
+
+    render(
+        <ProductPrice price={price} />
+    );
+
+    const currencyElement = screen.getByText('R$');
+    const priceElement = screen.getByText('1,200.00'); 
+
+    expect(currencyElement).toBeInTheDocument();
+    expect(priceElement).toBeInTheDocument();
+});
+
+test('should render ProductPrice with price equals zero', () => {
+    const price = 0;
+
+    render(
+        <ProductPrice price={price} />
+    );
+
+    const currencyElement = screen.getByText('R$');
+    const priceElement = screen.getByText('0.00'); 
+
+    expect(currencyElement).toBeInTheDocument();
+    expect(priceElement).toBeInTheDocument();
+});
+
+test('should render ProductPrice without thousand separator', () => {
+    const price = 600;
+
+    render(
+        <ProductPrice price={price} />
+    );
+
+    const currencyElement = screen.getByText('R$');
+    const priceElement = screen.getByText('600.00'); 
+
+    expect(currencyElement).toBeInTheDocument();
+    expect(priceElement).toBeInTheDocument();
+});
