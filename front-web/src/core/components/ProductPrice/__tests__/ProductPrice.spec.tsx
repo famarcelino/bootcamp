a29991cc23960329bcf9d252a1bf4aa80a149a23
Francisco de Assis Marcelino <famarcelino@gmail.com>
2020-12-04 09:53:24 -0300

    Add ButtonIcon tests

--- front-web/src/core/components/ProductPrice/__tests__/ProductPrice.spec.tsx
+++ front-web/src/core/components/ProductPrice/__tests__/ProductPrice.spec.tsx
@@ -2,44 +2,50 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import ProductPrice from '..';
 
-test('should render ProductPrice', () => {
-    const price = 1200;
+describe('Tests with price greather than zero', () => {
+    test('should render ProductPrice', () => {
+        const price = 1200;
+    
+        render(
+            <ProductPrice price={price} />
+        );
+    
+        const currencyElement = screen.getByText('R$');
+        const priceElement = screen.getByText('1,200.00'); 
+    
+        expect(currencyElement).toBeInTheDocument();
+        expect(priceElement).toBeInTheDocument();
+    });
 
-    render(
-        <ProductPrice price={price} />
-    );
+    test('should render ProductPrice without thousand separator', () => {
+        const price = 600;
+    
+        render(
+            <ProductPrice price={price} />
+        );
+    
+        const currencyElement = screen.getByText('R$');
+        const priceElement = screen.getByText('600.00'); 
+    
+        expect(currencyElement).toBeInTheDocument();
+        expect(priceElement).toBeInTheDocument();
+    });
+})
 
-    const currencyElement = screen.getByText('R$');
-    const priceElement = screen.getByText('1,200.00'); 
+describe('Tests with price lower or equal than zero', () => {
+    test('should render ProductPrice with price equals zero', () => {
+        const price = 0;
+    
+        render(
+            <ProductPrice price={price} />
+        );
+    
+        const currencyElement = screen.getByText('R$');
+        const priceElement = screen.getByText('0.00'); 
+    
+        expect(currencyElement).toBeInTheDocument();
+        expect(priceElement).toBeInTheDocument();
+    });
+})
 
-    expect(currencyElement).toBeInTheDocument();
-    expect(priceElement).toBeInTheDocument();
-});
 
-test('should render ProductPrice with price equals zero', () => {
-    const price = 0;
-
-    render(
-        <ProductPrice price={price} />
-    );
-
-    const currencyElement = screen.getByText('R$');
-    const priceElement = screen.getByText('0.00'); 
-
-    expect(currencyElement).toBeInTheDocument();
-    expect(priceElement).toBeInTheDocument();
-});
-
-test('should render ProductPrice without thousand separator', () => {
-    const price = 600;
-
-    render(
-        <ProductPrice price={price} />
-    );
-
-    const currencyElement = screen.getByText('R$');
-    const priceElement = screen.getByText('600.00'); 
-
-    expect(currencyElement).toBeInTheDocument();
-    expect(priceElement).toBeInTheDocument();
-});
