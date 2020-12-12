d47a42a3615e1c1a1ac23faba5ad02c9b89624ee
Francisco de Assis Marcelino <famarcelino@gmail.com>
2020-12-12 07:15:16 -0300

    Test Form SaveProduct

--- front-web/src/pages/Admin/components/Products/Form/index.tsx
+++ front-web/src/pages/Admin/components/Products/Form/index.tsx
@@ -82,6 +82,7 @@ const Form = () => {
                                 type="text"
                                 className="form-control input-base"
                                 placeholder="Nome do Produto"
+                                data-testid="name"
                             />
                             {errors.name && (
                                 <div className="invalid-feedback d-block">
@@ -90,6 +91,7 @@ const Form = () => {
                             )}
                         </div>
                         <div className="margin-bottom-30">
+                            <label htmlFor="categories" className="d-none">Categorias</label>
                             <Controller
                                 as={Select}
                                 name="categories"
@@ -102,6 +104,8 @@ const Form = () => {
                                 classNamePrefix="categories-select"
                                 placeholder="Categorias"
                                 isMulti
+                                inputId="categories"
+                                defaultValue=""
                             />
                             {errors.categories && (
                                 <div className="invalid-feedback d-block">
@@ -116,6 +120,7 @@ const Form = () => {
                                 type="number"
                                 className="form-control input-base"
                                 placeholder="Preço"
+                                data-testid="price"
                             />
                             {errors.price && (
                                 <div className="invalid-feedback d-block">
@@ -130,6 +135,7 @@ const Form = () => {
                                 type="text"
                                 className="form-control input-base"
                                 placeholder="Imagem do Produto"
+                                data-testid="imgUrl"
                             />
                             {errors.imgUrl && (
                                 <div className="invalid-feedback d-block">
@@ -147,6 +153,7 @@ const Form = () => {
                             cols={30}
                             rows={10}
                             placeholder="Descrição"
+                            data-testid="description"
                         />
                         {errors.description && (
                             <div className="invalid-feedback d-block">
