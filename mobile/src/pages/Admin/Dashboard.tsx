2c37de6694c231ef55e29d09459a7796585309ef
Francisco de Assis Marcelino <famarcelino@gmail.com>
2020-12-19 16:36:48 -0300

    Add ProductForm

--- mobile/src/pages/Admin/Dashboard.tsx
+++ mobile/src/pages/Admin/Dashboard.tsx
@@ -2,8 +2,10 @@ import React, {useState} from 'react';
 import { View } from 'react-native';
 import { TabBar } from '../../components';
 import Categories from './Categories';   
-import Products from './Products';   
 import Users from './Users';
+//Products
+import Products from './Products/ListProducts';
+import FormProduct from './Products/FormProduct';
 
 const Dashboard: React.FC = () => {
     const [screen, setScreen] = useState("products");
@@ -11,7 +13,8 @@ const Dashboard: React.FC = () => {
     return (
         <View>
             <TabBar screen={screen} setScreen={setScreen} />
-            {screen === "products" && <Products />}
+            {screen === "products" && <Products setScreen={setScreen} />}
+            {screen == "newProduct" && <FormProduct setScreen={setScreen} />}
             {screen === "categories" && <Categories />}
             {screen === "users" && <Users />}
         </View>
--- mobile/src/pages/Admin/Products/ListProducts.tsx
+++ mobile/src/pages/Admin/Products/ListProducts.tsx
@@ -1,13 +1,19 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, ScrollView, TouchableOpacity, ActivityIndicator, ActivityIndicatorComponent } from 'react-native';
-import { SearchInput, ProductCard } from '../../components';
-import { getProducts } from '../../services';
-import { admin, text } from '../../styles';
+import { Text, ScrollView, TouchableOpacity, ActivityIndicator } from 'react-native';
+import { SearchInput, ProductCard } from '../../../components';
+import { getProducts } from '../../../services';
+import { admin, text } from '../../../styles';
 
-const Products: React.FC = () => {
+interface ProductProps {
+    setScreen: Function;
+};
+
+const Products: React.FC<ProductProps> = (props) => {
     const [search, setSearch] = useState("");
     const [products, setProducts] = useState([]);
     const [loading, setLoading] = useState(false);
+
+    const {setScreen} = props;
 
     async function fillProducts() {
         setLoading(true);
@@ -26,7 +32,7 @@ const Products: React.FC = () => {
 
     return (
         <ScrollView contentContainerStyle={admin.container}>
-            <TouchableOpacity style={admin.addButton}>
+            <TouchableOpacity style={admin.addButton} onPress={() => setScreen("newProduct")}>
                 <Text style={text.addButtonText}>Adicionar</Text>
             </TouchableOpacity>
             <SearchInput search={search} setSearch={setSearch} placeholder="Nome do produto" />
