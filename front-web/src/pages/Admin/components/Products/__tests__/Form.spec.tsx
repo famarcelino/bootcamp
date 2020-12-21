81449d8d653c1f0228eb63fe24e318506ef48807
Francisco de Assis Marcelino <famarcelino@gmail.com>
2020-12-21 15:50:50 -0300

    Finish product form tests

--- front-web/src/pages/Admin/components/Products/__tests__/Form.spec.tsx
+++ front-web/src/pages/Admin/components/Products/__tests__/Form.spec.tsx
@@ -3,18 +3,16 @@ import { render, screen, waitFor } from '@testing-library/react';
 import selectEvent from 'react-select-event';
 import userEvent from '@testing-library/user-event';
 import Form from '../Form';
-import { Router } from 'react-router-dom';
+import { Router, useParams } from 'react-router-dom';
 import history from 'core/utils/history';
 import { rest } from 'msw';
 import { setupServer } from 'msw/node';
-import { categoriesResponse } from './fixtures';
+import { categoriesResponse, fillFormData, productResponse } from './fixtures';
 import { ToastContainer } from 'react-toastify';
 
 jest.mock('react-router-dom', () => ({
     ...jest.requireActual('react-router-dom'),
-    useParams: () => ({
-        productId: 'create'
-    })
+    useParams: jest.fn()
 }))
 
 const server = setupServer(
@@ -23,6 +21,12 @@ const server = setupServer(
     }),
     rest.post('http://localhost:8080/products', (req, res, ctx) => {
         return res(ctx.status(201))
+    }),
+    rest.get('http://localhost:8080/products/:productId', (req, res, ctx) => {
+        return res(ctx.json(productResponse))
+    }),
+    rest.put('http://localhost:8080/products/:productId', (req, res, ctx) => {
+        return res(ctx.status(200))
     })
 );
 
@@ -30,57 +34,107 @@ beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
 
-test('Should render Form', async () => {
-    render(
-        <Router history={history}>
-            <ToastContainer />
-            <Form />
-        </Router>
-    );
+describe('Creating a product', () => {
+    beforeEach(() => {
+        (useParams as jest.Mock).mockReturnValue({
+            productId: 'create'
+        })
+    });
 
-    const submitButton = screen.getByRole('button', { name: /salvar/i });
-    const nameInput = screen.getByTestId('name');
-    const priceInput = screen.getByTestId('price');
-    const imgUrlInput = screen.getByTestId('imgUrl');
-    const descriptionInput = screen.getByTestId('description');
-    const categoriesInput = screen.getByLabelText('Categorias');
-
-    userEvent.type(nameInput, 'Computador');
-    await selectEvent.select(categoriesInput, ['Computadores', 'Eletrônicos']);
-    userEvent.type(priceInput, '5000');
-    userEvent.type(imgUrlInput, 'image.jpg');
-    userEvent.type(descriptionInput, 'Ótimo Computador');
-
-    userEvent.click(submitButton);
-
-    await waitFor(() => expect(screen.getByText('Produto salvo com sucesso!')).toBeInTheDocument());
-    expect(history.location.pathname).toBe('/admin/products');
-    expect(screen.getByText(/cadastrar produto/i)).toBeInTheDocument();
+    test('Should render Form and submit with success', async () => {
+        render(
+            <Router history={history}>
+                <ToastContainer />
+                <Form />
+            </Router>
+        );
+    
+        const submitButton = screen.getByRole('button', { name: /salvar/i });
+        const categoriesInput = screen.getByLabelText('Categorias');
+        await selectEvent.select(categoriesInput, ['Computadores', 'Eletrônicos']);
+    
+        fillFormData();
+    
+        userEvent.click(submitButton);
+    
+        await waitFor(() => expect(screen.getByText('Produto salvo com sucesso!')).toBeInTheDocument());
+        expect(history.location.pathname).toBe('/admin/products');
+        expect(screen.getByText(/cadastrar produto/i)).toBeInTheDocument();
+    });
+    
+    test('Should render Form and submit with error', async () => {
+        server.use(
+            rest.post('http://localhost:8080/products', (req, res, ctx) => {
+              return res(ctx.status(500))
+            })
+          )
+    
+        render(
+            <Router history={history}>
+                <ToastContainer />
+                <Form />
+            </Router>
+        );
+    
+        const submitButton = screen.getByRole('button', { name: /salvar/i });
+        const categoriesInput = screen.getByLabelText('Categorias');
+        await selectEvent.select(categoriesInput, ['Computadores', 'Eletrônicos']);
+    
+        fillFormData();
+    
+        userEvent.click(submitButton);
+    
+        await waitFor(() => expect(screen.getByText('Erro ao salvar produto.')).toBeInTheDocument());
+        expect(history.location.pathname).toBe('/admin/products');
+        expect(screen.getByText(/cadastrar produto/i)).toBeInTheDocument();
+    });
+    
+    test('Should render Form and show validations', async () => {
+        render(
+            <Router history={history}>
+                <Form />
+            </Router>
+        );
+    
+        const submitButton = screen.getByRole('button', { name: /salvar/i });
+        userEvent.click(submitButton);
+    
+        await waitFor(() => expect(screen.getAllByText('Campo obrigatório')).toHaveLength(5));
+        const categoriesInput = screen.getByLabelText('Categorias');
+        await selectEvent.select(categoriesInput, ['Computadores', 'Eletrônicos']);
+    
+        fillFormData();
+    
+        await waitFor(() => expect(screen.queryAllByText('Campo obrigatório')).toHaveLength(0));
+    });
 });
 
-test('Should render Form without fill required fields', async () => {
-    render(
-        <Router history={history}>
-            <Form />
-        </Router>
-    );
+describe('Editing a product', () => {
+    beforeEach(() => {
+        (useParams as jest.Mock).mockReturnValue({
+            productId: '10'
+        })
+    });
 
-    const submitButton = screen.getByRole('button', { name: /salvar/i });
-    userEvent.click(submitButton);
+    test('Should render Form and submit with success', async () => {
+        render(
+            <Router history={history}>
+                <ToastContainer />
+                <Form />
+            </Router>
+        );
 
-    await waitFor(() => expect(screen.getAllByText('Campo obrigatório')).toHaveLength(5));
+        const submitButton = screen.getByRole('button', { name: /salvar/i });
+    
+        await waitFor(() => expect(screen.getByTestId('name')).toHaveValue('Macbook Pro'));
+        expect(screen.getByText('Computadores')).toBeInTheDocument();
+        expect(screen.getByText('Eletrônicos')).toBeInTheDocument();
+        expect(screen.getByTestId('price')).toHaveValue(1250);
+        expect(screen.getByTestId('imgUrl')).toHaveValue('image.jpg');
+        expect(screen.getByTestId('description')).toHaveValue('=)');
+        expect(screen.getByText(/editar produto/i)).toBeInTheDocument();
 
-    const nameInput = screen.getByTestId('name');
-    const priceInput = screen.getByTestId('price');
-    const imgUrlInput = screen.getByTestId('imgUrl');
-    const descriptionInput = screen.getByTestId('description');
-    const categoriesInput = screen.getByLabelText('Categorias');
-
-    userEvent.type(nameInput, 'Computador');
-    await selectEvent.select(categoriesInput, ['Computadores', 'Eletrônicos']);
-    userEvent.type(priceInput, '5000');
-    userEvent.type(imgUrlInput, 'image.jpg');
-    userEvent.type(descriptionInput, 'Ótimo Computador');
-
-    await waitFor(() => expect(screen.queryAllByText('Campo obrigatório')).toHaveLength(0));
+        userEvent.click(submitButton);
+        await waitFor(() => expect(screen.getByText('Produto salvo com sucesso!')).toBeInTheDocument());
+    });
 });
--- front-web/src/pages/Admin/components/Products/__tests__/fixtures.ts
+++ front-web/src/pages/Admin/components/Products/__tests__/fixtures.ts
@@ -1,3 +1,6 @@
+import { screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
 export const categoriesResponse = {
     "content": [
         {
@@ -13,4 +16,35 @@ export const categoriesResponse = {
             "name": "Livros"
         }
     ]
+};
+
+export const productResponse = {
+    "id": 3,
+    "name": "Macbook Pro",
+    "description": "=)",
+    "price": 1250.0,
+    "imgUrl": "image.jpg",
+    "date": "2020-07-14T10:00:00Z",
+    "categories": [
+        {
+            "id": 1,
+            "name": "Computadores"
+        },
+        {
+            "id": 2,
+            "name": "Eletrônicos"
+        }
+    ]
+};
+
+export const fillFormData = () => {
+    const nameInput = screen.getByTestId('name');
+    const priceInput = screen.getByTestId('price');
+    const imgUrlInput = screen.getByTestId('imgUrl');
+    const descriptionInput = screen.getByTestId('description');
+
+    userEvent.type(nameInput, 'Computador');
+    userEvent.type(priceInput, '5000');
+    userEvent.type(imgUrlInput, 'image.jpg');
+    userEvent.type(descriptionInput, 'Ótimo Computador');
 };
